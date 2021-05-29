ab127779af20a32ed4ffe676025f87c5e503cd30
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-05-30 07:35:57 +0800

    Displaying a log file in real time3

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -2,13 +2,13 @@ const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector('#log-window');
 const filePath = document.getElementById('logFilePath');
 
-connection.onopen = function(event) {
-    connection.send("Hello from the client!");
-    if (filePath != undefined) {
-        connection.send(filePath)
+connection.onopen = () => {
+    if (filePath) {
+      connection.send(filePath)
     }
-  };
+  }
 connection.onmessage = function(event){
     let str = event.data.toString('utf8');
     const logs = str.split("\n").join("<hr>");
+    logWindow.innerHTML = logs;
 };
